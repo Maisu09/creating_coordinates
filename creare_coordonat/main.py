520e0764f2df4b1e8bfd834c165c324730f967ec
Maisu09 <mflorian90@yahoo.com>
2023-10-20 14:42:11 +0300

    f

--- creare_coordonat/main.py
+++ creare_coordonat/main.py
@@ -1,1 +1,25 @@
 import cv2 as cv
+import os
+
+def change_dir_picture():
+    '''Changing wd to the directory where images are stored.'''
+    pass
+    # if os.getcwd() == r'../crerare_coordonate':
+    #     os.chdir(r'/Licenta/poze')
+    #
+    # print((os.getcwd()))
+    #
+
+
+def main():
+    img = cv.imread('face1.jpg', cv.IMREAD_COLOR)
+
+    cv.imshow('image', img)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+
+    print(change_dir_picture.__doc__)
+
+
+if __name__ == "__main__":
+    main()
