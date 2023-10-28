58991997920b66a2ee4e0a80f85d5e45a907747f
Maisu09 <mflorian90@yahoo.com>
2023-10-28 12:29:35 +0300

    changing directory and saving the processed image. organized functions. In algTry demo GUI.

--- creare_coordonat/algTry.py
+++ creare_coordonat/algTry.py
@@ -41,26 +41,96 @@ import numpy as np
 #
 #
 
-img = cv2.imread('face1.jpg')
-# gray = cv2.cvtColor(img, cv.COLOR_BGR2GRAY)
 
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# # Generare puncte prin contours.
+# img = cv2.imread('unu.jpg')
+# # gray = cv2.cvtColor(img, cv.COLOR_BGR2GRAY)
+#
+# gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+#
+# ret, threshold = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+# threshold = 255 - threshold
+#
+# contours, hierarchy = cv2.findContours(threshold, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+#
+# cv2.imwrite('box_contour.jpg', img)
+#
+# img_copy = img.copy()
+# cv2.drawContours(img_copy, contours, -1, (0, 255, 0), 3)
+#
+# cv2.imshow('threshold', threshold)
+# cv2.imshow('image', img_copy)
+# cv2.waitKey(0)
 
-ret, threshold = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-threshold = 255 - threshold
+def GUI1():
+    # GUI
 
-# OR use inRange()
-#lower=(70,60,60)
-#upper=(90,80,80)
-#threshold = cv2.inRange(img, lower, upper)
+    import PySimpleGUI as sg
+    import os.path
 
-contours, hierarchy = cv2.findContours(threshold,  cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    # First the window layout in 2 columns
 
-cv2.imwrite('box_contour.jpg', img)
+    file_list_column = [
+        [
+            sg.Text("Image Folder"),
+            sg.In(size=(25, 1), enable_events=True, key="-FOLDER-"),
+            sg.FolderBrowse(),
+        ],
+        [
+            sg.Listbox(
+                values=[], enable_events=True, size=(40, 20), key="-FILE LIST-"
+            )
+        ],
+    ]
 
-img_copy=img.copy()
-cv2.drawContours(img_copy, contours, -1, (0, 255, 0), 3)
+    # For now will only show the name of the file that was chosen
+    image_viewer_column = [
+        [sg.Text("Choose an image from list on left:")],
+        [sg.Text(size=(40, 1), key="-TOUT-")],
+        [sg.Image(key="-IMAGE-")],
+    ]
 
-cv2.imshow('threshold', threshold)
-cv2.imshow('image', img_copy)
-cv2.waitKey(0)
+    # ----- Full layout -----
+    layout = [
+        [
+            sg.Column(file_list_column),
+            sg.VSeperator(),
+            sg.Column(image_viewer_column),
+        ]
+    ]
+
+    window = sg.Window("Image Viewer", layout)
+
+    # Run the Event Loop
+    while True:
+        event, values = window.read()
+        if event == "Exit" or event == sg.WIN_CLOSED:
+            break
+        # Folder name was filled in, make a list of files in the folder
+        if event == "-FOLDER-":
+            folder = values["-FOLDER-"]
+            try:
+                # Get list of files in folder
+                file_list = os.listdir(folder)
+            except:
+                file_list = []
+
+            fnames = [
+                f
+                for f in file_list
+                if os.path.isfile(os.path.join(folder, f))
+                and f.lower().endswith((".png", ".jpg"))
+            ]
+            window["-FILE LIST-"].update(fnames)
+        elif event == "-FILE LIST-":  # A file was chosen from the listbox
+            try:
+                filename = os.path.join(
+                    values["-FOLDER-"], values["-FILE LIST-"][0]
+                )
+                window["-TOUT-"].update(filename)
+                window["-IMAGE-"].update(filename=filename)
+
+            except:
+                pass
+
+    window.close()
