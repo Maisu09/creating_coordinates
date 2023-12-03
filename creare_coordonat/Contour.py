e163affe34c3a63f704f842b5bbd7db4881b6d34
Maisu09 <mflorian90@yahoo.com>
2023-12-03 09:48:02 +0200

    Made private variables.

--- creare_coordonat/Contour.py
+++ creare_coordonat/Contour.py
@@ -4,14 +4,15 @@ import matplotlib.pyplot as plt
 
 
 class Contour:
-    def __init__(self, image):
-        self.image = image
+    def __init__(self, image, points: dict):
+        self.__image = image
+        self.__points = points
 
-    def polynom_draw(self, points: dict, window_name):
+    def polynom_draw(self, window_name):
         """Primeste doua puncte din dictionar si creaza linia."""
         list_of_polynomial_points = []
-        x_p1, y_p1 = points['p1']
-        x_p2, y_p2 = points['p2']
+        x_p1, y_p1 = self.__points['p1']
+        x_p2, y_p2 = self.__points['p2']
 
         for i in range(0, 100, 1):
             i = i / 100
@@ -22,7 +23,7 @@ class Contour:
             tuple_point = (x, y)
             list_of_polynomial_points.append(tuple_point)
 
-        image_rgb = cv2.cvtColor(self.image, cv2.COLOR_BGR2RGB)
+        image_rgb = cv2.cvtColor(self.__image, cv2.COLOR_BGR2RGB)
         plt.scatter([p[0] for p in list_of_polynomial_points], [p[1] for p in list_of_polynomial_points], color='red')
         plt.imshow(image_rgb)
         plt.title(window_name)
