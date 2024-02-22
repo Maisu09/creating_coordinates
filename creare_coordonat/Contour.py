0a2f3949fa898200e3fa71fe267af1787360e12a
Maisu09 <mflorian90@yahoo.com>
2024-02-22 18:35:13 +0200

    Added a 3rd degree polinom as the way to connect the points

--- creare_coordonat/Contour.py
+++ creare_coordonat/Contour.py
@@ -11,32 +11,71 @@ class Contour:
     def __init__(self, face_one: Face, face_two: Face):
         self._face_one = face_one
         self._face_two = face_two
-        # self._points1 = points1
-        # self._points2 = points2
+        self._polinom = []
         self._speed = []
 
-    @staticmethod
-    def line(self, points, image, window_name):
-        list_of_polynomial_points = []
+    def solving_polinom(self, points):
+        #3rd degree polinom : x(t)=a+bt+ct^2+dt^3, y(t)=a1+b1t+c1t^2+d1t^3
 
-        for j in range(len(points) - 1):
+        v1x = 0
+        v1y = 0
+        v2x = 0
+        v2y = 0
+        for i in range(len(points) - 1):
+            x_p1, y_p1, t_p1 = points['p' + str(i)]
+            x_p2, y_p2, t_p2 = points['p' + str(i + 1)]
 
-            x_p1, y_p1, t_p1 = points['p' + str(j)]
-            x_p2, y_p2, t_p2 = points['p' + str(j + 1)]
+            ax = x_p1
+            bx = v1x
+            dx = v2x - 2*x_p2 + 2*x_p1 + v1x
+            cx = x_p2 - x_p1 - v1x - dx
 
-            for i in range(0, 100, 1):
-                i = i / 100
+            ay = y_p1
+            by = v1y
+            dy = v2y - 2*y_p2 + 2*y_p1 + v1y
+            cy = y_p2 - y_p1 - v1y -dy
 
-                x = x_p1 + (x_p2 - x_p1) * i
-                y = y_p1 + (y_p2 - y_p1) * i
-                t = t_p1 + (t_p2 - t_p1) * i
-                # print(x, y, t)
+            for t in range(0, 100, 1):
+                t = t/ 100
+                x = float(ax + bx*t + cx*t*t + dx*t*t*t)
+                y = float(ay + by*t + cy*t*t + dy*t*t*t)
+                
                 tuple_point = (x, y, t)
-                cv2.circle(image,
-                           (round(tuple_point[0]), (round(tuple_point[1]))),
-                           2, [255, 0, 0], -1
-                           )
-                list_of_polynomial_points.append(tuple_point)
+                self._polinom.append(tuple_point)
+
+    # @staticmethod
+    def line(self, points, image, window_name):
+        # list_of_polynomial_points = []
+
+        # for j in range(len(points) - 1):
+
+        #     x_p1, y_p1, t_p1 = points['p' + str(j)]
+        #     x_p2, y_p2, t_p2 = points['p' + str(j + 1)]
+        #     #calcul coef
+
+        #     for i in range(0, 100, 1):
+        #         i = i / 100
+
+        #         x = x_p1 + (x_p2 - x_p1) * i
+        #         y = y_p1 + (y_p2 - y_p1) * i
+        #         t = t_p1 + (t_p2 - t_p1) * i
+        #         # print(x, y, t)
+        #         tuple_point = (x, y, t)
+        #         cv2.circle(image,
+        #                    (round(tuple_point[0]), (round(tuple_point[1]))),
+        #                    2, [255, 0, 0], -1
+        #                    )
+        #         list_of_polynomial_points.append(tuple_point)
+        self.solving_polinom(points)
+
+        for elems in self._polinom:
+            cv2.circle(
+                        image, (round(elems[0]), round(elems[1])),
+                        2, [255, 0, 0], -1
+            )
+
+        cv2.imshow(window_name, image)
+
         # cv2.imshow(window_name, image)
         # self.speed(list_of_polynomial_points)
         #
@@ -56,7 +95,7 @@ class Contour:
         # Update the original images with the copies
         # self._face_one.image[:] = image_one_copy
         # self._face_two.image[:] = image_two_copy
-        #
+        # #
         # self.line(self._points1, self._face_one.image)
         # self.line(self._points2, self._face_two.image)
 
